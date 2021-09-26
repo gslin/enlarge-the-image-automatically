6e0efce9ca9cad175491390cdb673c5ee0c8296d
Gea-Suan Lin <gslin@gslin.org>
2021-09-26 10:02:15 +0800

    Add picdollar.com.

--- enlarge-the-image-automatically.user.js
+++ enlarge-the-image-automatically.user.js
@@ -5,8 +5,9 @@
 // @match       https://imgsto.com/img/*.jpg
 // @match       https://www.picbaron.com/img/*.jpeg
 // @match       https://pics4you.net/img/*.jpg
+// @match       https://picdollar.com/img/*.jpg
 // @grant       none
-// @version     0.20210920.0
+// @version     0.20210926.0
 // @author      Gea-Suan Lin <gslin@gslin.com>
 // @description Enlarge the image automatically.
 // @license     MIT
