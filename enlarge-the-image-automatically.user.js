93cd2cee071c1d3caafc811c2538f18f3320b520
Gea-Suan Lin <gslin@gslin.org>
2021-09-29 05:27:36 +0800

    Add support for imgbaron.com.

--- enlarge-the-image-automatically.user.js
+++ enlarge-the-image-automatically.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name        Enlarge the image automatically
 // @namespace   https://github.com/gslin/enlarge-the-image-automatically
+// @match       https://imgbaron.com/img/*.jpeg
 // @match       https://imgsen.com/img/*.jpg
 // @match       https://imgsto.com/img/*.jpg
 // @match       https://www.picbaron.com/img/*.jpeg
 // @match       https://pics4you.net/img/*.jpg
 // @match       https://picdollar.com/img/*.jpg
 // @grant       none
-// @version     0.20210926.0
+// @version     0.20210929.0
 // @author      Gea-Suan Lin <gslin@gslin.com>
 // @description Enlarge the image automatically.
 // @license     MIT
