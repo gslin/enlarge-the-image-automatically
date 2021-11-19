82d99ffe2f50e7e43ed6caf438752f4e03d4919e
Gea-Suan Lin <gslin@gslin.org>
2021-11-20 02:08:51 +0800

    Sort & add kropic.com.

--- enlarge-the-image-automatically.user.js
+++ enlarge-the-image-automatically.user.js
@@ -4,12 +4,13 @@
 // @match       https://imgbaron.com/img/*.jpeg
 // @match       https://imgsen.com/img/*.jpg
 // @match       https://imgsto.com/img/*.jpg
-// @match       https://www.picbaron.com/img/*.jpeg
+// @match       https://kropic.com/img/*.jpg
 // @match       https://pics4you.net/img/*.jpg
 // @match       https://picdollar.com/img/*.jpg
 // @match       https://silverpic.com/img/*.jpg
+// @match       https://www.picbaron.com/img/*.jpeg
 // @grant       none
-// @version     0.20211020.0
+// @version     0.20211120.0
 // @author      Gea-Suan Lin <gslin@gslin.com>
 // @description Enlarge the image automatically.
 // @license     MIT
