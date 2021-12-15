131695a4a561b7298756ba55a53a5cb0fdd880b0
Gea-Suan Lin <gslin@gslin.org>
2021-12-16 01:48:34 +0800

    Add more patterns.

--- enlarge-the-image-automatically.user.js
+++ enlarge-the-image-automatically.user.js
@@ -3,15 +3,22 @@
 // @namespace   https://github.com/gslin/enlarge-the-image-automatically
 // @match       https://imgbaron.com/img/*.jpeg
 // @match       https://imgbaron.com/img/*.jpg
+// @match       https://imgsen.com/img/*.jpeg
 // @match       https://imgsen.com/img/*.jpg
+// @match       https://imgsto.com/img/*.jpeg
 // @match       https://imgsto.com/img/*.jpg
+// @match       https://kropic.com/img/*.jpeg
 // @match       https://kropic.com/img/*.jpg
+// @match       https://pics4you.net/img/*.jpeg
 // @match       https://pics4you.net/img/*.jpg
+// @match       https://picdollar.com/img/*.jpeg
 // @match       https://picdollar.com/img/*.jpg
+// @match       https://silverpic.com/img/*.jpeg
 // @match       https://silverpic.com/img/*.jpg
 // @match       https://www.picbaron.com/img/*.jpeg
+// @match       https://www.picbaron.com/img/*.jpg
 // @grant       none
-// @version     0.20211120.1
+// @version     0.20211216.0
 // @author      Gea-Suan Lin <gslin@gslin.com>
 // @description Enlarge the image automatically.
 // @license     MIT
